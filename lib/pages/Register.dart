a694a73a79f108c50236fcd8955e50a88ed26cbf
Panidaeiei <161023725+Panidaeiei@users.noreply.github.com>
2024-08-13 16:36:51 +0700

    fim-register

--- lib/pages/Register.dart
+++ lib/pages/Register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minipro_mba/pages/login.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -19,25 +20,215 @@ class _RegisterState extends State<Register> {
           child: Row(
             children: <Widget>[
               Image.asset(
-                'assets/images/ICON.png', 
+                'assets/images/ICON.png',
                 height: 60,
               ),
               SizedBox(width: 5), // เพิ่มระยะห่างระหว่างรูปภาพและข้อความ
               const Text(
-                'สมัครสมาชิก', 
+                'สมัครสมาชิก',
                 style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(255, 255, 255, 2)
-                ),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(255, 255, 255, 2)),
               ),
             ],
           ),
         ),
       ),
-      body: Container(
-        color: Color.fromARGB(0, 0, 0, 0),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color.fromARGB(0, 0, 0, 0),
+          child: Padding(
+            padding: EdgeInsets.only(top: 30, left: 50, right: 50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('ชื่อ-สกุล : '),
+                SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 247, 127, 118),
+                          width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text('อีเมลล์ : '),
+                ),
+                SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 247, 127, 118),
+                          width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text('เบอร์โทรศัพท์ : '),
+                ),
+                SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 247, 127, 118),
+                          width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text('รหัสผ่าน : '),
+                ),
+                SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 247, 127, 118),
+                          width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text('จำนวนเงินในวอลเล็ท : '),
+                ),
+                SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 247, 127, 118),
+                          width: 2), // เปลี่ยนขอบเป็นสีแดงเมื่อคลิก
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Center(
+                      child: FilledButton(
+                    onPressed: regissuccess,
+                    child: Text('สมัครสมาชิก'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(255, 230, 92, 87)),
+                    ),
+                  )),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text('คุณมีบัญชีอยู่แล้ว ?'),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 9),
+                      child: TextButton(
+                          onPressed: gologin,
+                          child: const Text(
+                            'เข้าสู่ระบบ',
+                            style: TextStyle(
+                                color: Color.fromRGBO(221, 86, 76, 1)),
+                          )),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void regissuccess() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: const Color.fromARGB(255, 245, 156, 55),
+        title:  Center(
+          child:  Column(
+            children: [
+              Image.asset('assets/images//check.png',
+                width: 70,
+              ),
+              const Text(
+                'สมัครสมาชิก',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  fontWeight: FontWeight.w800,
+                  fontSize: 30
+                ),
+              ),
+              const Text(
+                'สำเร็จ',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  fontWeight: FontWeight.w800,
+                  fontSize: 30
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          FilledButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LoginPage()));
+            },
+            child: Center(child: const Text('ตกลง',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w800,
+              ),
+            )),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  const Color.fromARGB(255, 230, 92, 87)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void gologin() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoginPage()));
   }
 }
