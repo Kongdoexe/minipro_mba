29ff4e97e31eacdcfc750c7ba7a46f5900fcbaf7
Kongnaaa <156898909+Kongdoexe@users.noreply.github.com>
2024-08-31 18:47:27 +0700

    Update BuyLotto.dart

--- lib/pages/User/BuyLotto.dart
+++ lib/pages/User/BuyLotto.dart
@@ -10,7 +10,7 @@ import 'package:http/http.dart' as http;
 class BuylottoPage extends StatefulWidget {
   // int memberId = 0;
   // BuylottoPage({Key? key, required this.memberId}) : super(key: key);
-  const BuylottoPage({super.key});
+  const BuylottoPage({super.key, required int memberId});
   @override
   State<BuylottoPage> createState() => _BuylottoPageState();
 }
